1471269913a2f2e6f369e98b023509ab7e99f202
priyammoradiya24 <167057027+priyammoradiya24@users.noreply.github.com>
2024-05-14 14:23:35 +0530

    Add files via upload

---
+++ priyam22.py
@@ -0,0 +1,35 @@
+#class and object
+
+class tg:
+    def config(self):
+        print("my name is hello")
+
+a=3
+print(a)
+
+hello=tg()
+sagar=tg()
+print(type(hello))
+
+
+class com:
+    def config(self):
+        print("my name is hello")
+    
+com1=com()
+com2=com()
+
+com.config(com1)
+com.config(com2)
+
+com1.config()
+
+
+class c:
+    def config(self):
+        for i in range(1000000000000000,100000000000000000000000000000000000000000000000):
+            print(i)
+        i=+100000000000000
+com1=c()
+com2=c()
+com1.config()
